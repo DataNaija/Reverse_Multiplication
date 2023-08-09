e9f31eec1377908a48d66d5b490ef8e64aa71b50
Muhydeen Adelani <139554441+DataNaija@users.noreply.github.com>
2023-08-09 00:32:43 -0600

    Update dy_01.py

--- dy_01.py
+++ dy_01.py
@@ -1,6 +1,3 @@
-# Can you modify our multiplication table program in dy_0.py,
-# so that we get a multiplication table from 10 to 1 instead of 1 to 10.
-
 number = int(input("Enter a number: "))
 i = 10
 
